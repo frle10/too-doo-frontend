32cf021c3f77e8ca55a35042f5bff4f523e83c67
Ivan Skorupan <ivan.skorupan@fer.hr>
2020-09-26 12:39:11 +0200

    Finished to-do generator component.

--- src/components/ToDoGenerator.tsx
+++ src/components/ToDoGenerator.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { css } from 'emotion';
+import Plus from '../images/plus.svg';
 
-interface Props {}
+const generatorStyle = css({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+});
 
-export const ToDoGenerator = (props: Props) => {
-  return <div></div>;
+const inputStyle = css({
+  width: '100%',
+  height: '56px',
+  borderRadius: '4px',
+  outline: 'none',
+  border: 'none',
+  background: 'rgba(0, 0, 0, 0.05)',
+  fontFamily: 'Inter',
+  fontSize: '21px',
+  color: 'rgba(0, 0, 0, 0.25)',
+  padding: '0 50px',
+  margin: '15px 0',
+});
+
+const plusStyle = css({
+  position: 'absolute',
+  padding: '0 20px',
+  cursor: 'pointer',
+});
+
+export const ToDoGenerator = () => {
+  return (
+    <div className={generatorStyle}>
+      <img src={Plus} alt='Plus' className={plusStyle} />
+      <input type='text' className={inputStyle} placeholder='Add a to-do...' />
+    </div>
+  );
 };
