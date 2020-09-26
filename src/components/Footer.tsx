b7ee4f0f9426fa876a9775d304a1085948878e80
Ivan Skorupan <ivan.skorupan@fer.hr>
2020-09-26 12:18:27 +0200

    Finished footer component.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { css } from 'emotion';
 
-interface Props {}
+const footerStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  color: 'rgba(0, 0, 0, 0.3)',
+});
 
-export const Footer = (props: Props) => {
-  return <div></div>;
+const separatorStyle = css({
+  width: '100%',
+  height: '1px',
+  background: 'rgba(0, 0, 0, 0.1)',
+  margin: '20px 0',
+});
+
+const footerHeaderStyle = css({
+  fontWeight: 900,
+  fontSize: '32px',
+  margin: '5px 0',
+});
+
+export const Footer = () => {
+  return (
+    <div className={footerStyle}>
+      <div className={separatorStyle}></div>
+      <div className={footerHeaderStyle}>Too Doo</div>
+      <div>Your to-dos have never been simpler.</div>
+    </div>
+  );
 };
