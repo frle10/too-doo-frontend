09ffd4dceafce7d045788e5222abc2de97060c55
Ivan Skorupan <ivan.skorupan@fer.hr>
2020-10-03 16:51:11 +0200

    Remove some request headers.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
       if (!urlUuid) {
         setToDoList(emptyList);
       } else {
-        const list = await axios.get(`${BACKEND_DOMAIN}/todos/${uuid}`, {headers: {'Access-Control-Allow-Origin': '*'}});
+        const list = await axios.get(`${BACKEND_DOMAIN}/todos/${uuid}`);
         if (list.data) {
           setToDoList(list.data);
         } else {
--- src/util/apiUtil.ts
+++ src/util/apiUtil.ts
@@ -2,15 +2,15 @@ import axios from 'axios';
 import { BACKEND_DOMAIN } from './constants';
 
 export const callChangeName = (uuid: string | undefined, name: string) => {
-  return axios.patch(`${BACKEND_DOMAIN}/todos/${uuid}`, { name }, {headers: {'Access-Control-Allow-Origin': '*'}});
+  return axios.patch(`${BACKEND_DOMAIN}/todos/${uuid}`, { name });
 };
 
 export const callAddTodo = (uuid: string | undefined, content: string) => {
   return axios.post(`${BACKEND_DOMAIN}/todos/todo/${uuid}`, {
     content,
-  }, {headers: {'Access-Control-Allow-Origin': '*'}});
+  });
 };
 
 export const callChangeCompleted = (id: number) => {
-  return axios.patch(`${BACKEND_DOMAIN}/todos/todo/${id}`, {}, {headers: {'Access-Control-Allow-Origin': '*'}});
+  return axios.patch(`${BACKEND_DOMAIN}/todos/todo/${id}`);
 };
