628865ac4c259ff8dcbf78d4021f0387c616d76c
Ivan Skorupan <ivan.skorupan@fer.hr>
2020-10-03 15:17:24 +0200

    HTTP to HTTPS

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,7 +1,7 @@
 import { TodoList } from './types';
 
 export const UNTITLED = 'untitled';
-export const BACKEND_DOMAIN = 'http://api.frle.net';
+export const BACKEND_DOMAIN = 'https://api.frle.net';
 
 export const toDoListExample: TodoList = {
   id: 1,
